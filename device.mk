203d31b2044e43d7499a106dee71a36a6ebf3247
Lukman Shaikh <shaikhlukman01@gmail.com>
2018-06-22 03:51:00 +0000

    Added Thermal

--- device.mk
+++ device.mk
@@ -383,6 +383,9 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-service \
     thermal.msm8937
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
