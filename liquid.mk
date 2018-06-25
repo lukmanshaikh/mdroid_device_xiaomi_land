23736ecc67e518170af183654cbcb777b8c08a52
Lukman Shaikh <shaikhlukman01@gmail.com>
2018-06-25 04:30:51 +0000

    Added maintainer

--- liquid.mk
+++ liquid.mk
@@ -36,6 +36,7 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR_PRODUCT_NAME := land
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="land-user 6.0.1 MMB29M V9.5.1.0.MALMIFA release-keys"
+    PRIVATE_BUILD_DESC="land-user 6.0.1 MMB29M V9.5.1.0.MALMIFA release-keys" \
+     DEVICE_MAINTAINERS="Lukman Shaikh"
 
 BUILD_FINGERPRINT := Xiaomi/land/land:6.0.1/MMB29M/V9.5.1.0.MALMIFA:user/release-keys
